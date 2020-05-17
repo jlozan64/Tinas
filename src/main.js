23e08f1ca6f05096372852d266e4f8f1d49336bd
jlozan64 <janeth2.jl@gmail.com>
2020-05-17 17:14:21 -0500

    remove tailwind

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "@/plugins/firebase";
-import "@/assets/tailwind.css";
 
 Vue.config.productionTip = false;
 
